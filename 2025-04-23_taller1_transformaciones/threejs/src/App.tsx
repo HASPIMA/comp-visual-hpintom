d681ba3a4d9a9e8e1c2ddce1f455385e9d855f2a
Harrison Pinto <hpintom@unal.edu.co>
2025-04-23 08:57:22 -0500

    Create a `Canvas` and `Box` component
    A `Box` or 3D cube is rendered almost in the center of the screen. The
    cube is blue and changes to pink when hovered over. The cube also scales
    up when clicked. The `Canvas` component is used to create a 3D scene
    using the `@react-three/fiber` library. The `Box` component is a
    custom component that uses the `mesh` element from Three.js to create a
    3D box. The `ambientLight`, `spotLight`, and `pointLight` components
    are used to add lighting to the scene. The `Box` component is positioned
    at `[-1.5, 0, 0]` in the 3D space.
    
    This closes #15

--- 2025-04-23_taller1_transformaciones/threejs/src/App.tsx
+++ 2025-04-23_taller1_transformaciones/threejs/src/App.tsx
@@ -1,5 +1,42 @@
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { Canvas, ThreeElements } from "@react-three/fiber";
+
+function Box(props: ThreeElements["mesh"]) {
+  const meshRef = useRef<THREE.Mesh>(null!);
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(_) => setActive(!active)}
+      onPointerOver={(_) => setHover(true)}
+      onPointerOut={(_) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "#2f74c0"} />
+    </mesh>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.5, 0, 0]} />
+    </Canvas>
+  );
 }
 
 export default App;
