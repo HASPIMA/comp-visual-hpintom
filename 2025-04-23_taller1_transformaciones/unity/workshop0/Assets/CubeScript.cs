bf723b4d6f3dbc9493a5f0328c9881e3a9099573
Harrison Pinto <hpintom@unal.edu.co>
2025-04-23 22:57:10 -0500

    Reformat `CubeScript.cs`
    Make conditionals more readable and maintainable

--- 2025-04-23_taller1_transformaciones/unity/workshop0/Assets/CubeScript.cs
+++ 2025-04-23_taller1_transformaciones/unity/workshop0/Assets/CubeScript.cs
@@ -11,20 +11,23 @@ public class CubeScript : MonoBehaviour
 
     void Update()
     {
+        // Reset position after 5 seconds
+        bool shouldReset = Time.frameCount % 300 == 0;
+
         // Random translation along the X or Y axis every few seconds
-        if (Time.frameCount % 60 == 0)
+        bool isNewFrameEvent = Time.frameCount % 80 == 0;
+        if (!shouldReset && isNewFrameEvent)
         {
             float randomX = Random.Range(-1f, 1f);
             float randomY = Random.Range(-1f, 1f);
             transform.Translate(new Vector3(randomX, randomY, 0));
             Debug.Log("New position: " + transform.position);
+        }
 
-            // Reset position after 5 seconds
-            if (Time.frameCount % 300 == 0)
-            {
-                transform.position = initialPosition;
-                Debug.Log("Position reset to: " + transform.position);
-            }
+        if (shouldReset)
+        {
+            transform.position = initialPosition;
+            Debug.Log("Position reset to the original one");
         }
 
         // Constant rotation dependent on Time.deltaTime
